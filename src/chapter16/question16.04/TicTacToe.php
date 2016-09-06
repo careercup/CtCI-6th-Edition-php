1d4844f05d5aa179579f86155668d10b60c9ed04
asaph <ace@dontletgo.com>
2016-09-05 20:57:48 -0700

    Add solution and unit tests for question 16.4

---
+++ src/chapter16/question16.04/TicTacToe.php
@@ -0,0 +1,85 @@
+<?php
+
+class TicTacToe {
+    const DEFAULT_DIMENSION = 3;
+    private $board;
+    private $rowSums;
+    private $colSums;
+    private $diagonalTopLeftToBottomRightSum;
+    private $diagonalBottomLeftToTopRightSum;
+    private $n;
+
+    public function __construct($n = self::DEFAULT_DIMENSION) {
+        $this->n = $n;
+        $this->board = [];
+        $this->rowSums = [];
+        $this->colSums = [];
+        for ($i=0; $i<$this->n; $i++) {
+            $this->rowSums[$i] = 0;
+            $this->colSums[$i] = 0;
+        }
+        $this->diagonalTopLeftToBottomRightSum = 0;
+        $this->diagonalBottomLeftToTopRightSum = 0;
+    }
+
+    public function makeMoveAndGetWinner($player, $row, $col) {
+        $this->setMove($player, $row, $col);
+        $delta = strtolower($player) == 'x' ? 1 : -1;
+        $this->rowSums[$row] += $delta;
+        $this->colSums[$col] += $delta;
+        if ($row == $col) {
+            $this->diagonalTopLeftToBottomRightSum += $delta;
+        }
+        if ($this->n - 1 - $row == $col) {
+            $this->diagonalBottomLeftToTopRightSum += $delta;
+        }
+        return $this->getWinner($row, $col);
+    }
+
+    private function getWinner($row, $col) {
+        if ($this->rowSums[$row] == $this->n
+            || $this->colSums[$col] == $this->n
+            || $this->diagonalTopLeftToBottomRightSum == $this->n
+            || $this->diagonalBottomLeftToTopRightSum == $this->n) {
+            return 'x';
+        } elseif ($this->rowSums[$row] == -$this->n
+            || $this->colSums[$col] == -$this->n
+            || $this->diagonalTopLeftToBottomRightSum == -$this->n
+            || $this->diagonalBottomLeftToTopRightSum == -$this->n) {
+            return 'o';
+        }
+        return null;
+    }
+
+    private function setMove($player, $row, $col) {
+        if ($this->getMove($row, $col) !== ' ') {
+            throw new InvalidArgumentException('position (' . $row . ',' . $col . ') is already occupied');
+        }
+        if (!array_key_exists($row, $this->board)) {
+            $this->board[$row] = [];
+        }
+        $this->board[$row][$col] = strtolower($player);
+    }
+
+    private function getMove($row, $col) {
+        return isset($this->board[$row][$col]) ? $this->board[$row][$col] : ' ';
+    }
+
+    public function __toString() {
+        $ticTacToe = '';
+        $horizontalDelimiter = '--' . str_repeat('+---', $this->n - 2) . '+--';
+        for ($row = 0; $row < $this->n; $row++) {
+            if ($row > 0) {
+                $ticTacToe .= $horizontalDelimiter . "\n";
+            }
+            for ($col = 0; $col < $this->n; $col++) {
+                if ($col > 0) {
+                    $ticTacToe .= ' | ';
+                }
+                $ticTacToe .= $this->getMove($row, $col);
+            }
+            $ticTacToe .= "\n";
+        }
+        return $ticTacToe;
+    }
+}
---
+++ test/chapter16/question16.04/TicTacToeTest.php
@@ -0,0 +1,178 @@
+<?php
+require_once __DIR__ . '/../../../src/chapter16/question16.04/TicTacToe.php';
+
+class TicTacToeTest extends PHPUnit_Framework_TestCase {
+
+    public function testIsXWinnerVertical() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 1, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 2));
+        $this->assertEquals('x', $ticTacToe->makeMoveAndGetWinner('x', 2, 0));
+        $expected = "x | o |  " . "\n"
+                  . "--+---+--" . "\n"
+                  . "x |   | o" . "\n"
+                  . "--+---+--" . "\n"
+                  . "x |   |  " . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIsOWinnerVertical() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 2));
+        $this->assertEquals('o', $ticTacToe->makeMoveAndGetWinner('o', 2, 0));
+        $expected = "o |   | x" . "\n"
+                  . "--+---+--" . "\n"
+                  . "o | x |  " . "\n"
+                  . "--+---+--" . "\n"
+                  . "o |   | x" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIsXWinnerHorizontal() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 0));
+        $this->assertEquals('x', $ticTacToe->makeMoveAndGetWinner('x', 2, 2));
+        $expected = "o | o |  " . "\n"
+                  . "--+---+--" . "\n"
+                  . "  |   |  " . "\n"
+                  . "--+---+--" . "\n"
+                  . "x | x | x" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIsOWinnerHorizontal() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 2));
+        $this->assertEquals('o', $ticTacToe->makeMoveAndGetWinner('o', 1, 2));
+        $expected = "  |   | x" . "\n"
+                  . "--+---+--" . "\n"
+                  . "o | o | o" . "\n"
+                  . "--+---+--" . "\n"
+                  . "x |   | x" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIsXWinnerDiagonalTopLeftToBottomRight() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 1));
+        $this->assertEquals('x', $ticTacToe->makeMoveAndGetWinner('x', 0, 0));
+        $expected = "x | o | o" . "\n"
+                  . "--+---+--" . "\n"
+                  . "  | x |  " . "\n"
+                  . "--+---+--" . "\n"
+                  . "  |   | x" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIsOWinnerDiagonalTopLeftToBottomRight() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 0));
+        $this->assertEquals('o', $ticTacToe->makeMoveAndGetWinner('o', 2, 2));
+        $expected = "o | x | x" . "\n"
+                  . "--+---+--" . "\n"
+                  . "  | o |  " . "\n"
+                  . "--+---+--" . "\n"
+                  . "x |   | o" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIsXWinnerDiagonalBottomLeftToTopRight() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 2, 1));
+        $this->assertEquals('x', $ticTacToe->makeMoveAndGetWinner('x', 0, 2));
+        $expected = "  | o | x" . "\n"
+                  . "--+---+--" . "\n"
+                  . "  | x |  " . "\n"
+                  . "--+---+--" . "\n"
+                  . "x | o |  " . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIsOWinnerDiagonalBottomLeftToTopRight() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 1, 2));
+        $this->assertEquals('o', $ticTacToe->makeMoveAndGetWinner('o', 2, 0));
+        $expected = "x |   | o" . "\n"
+                  . "--+---+--" . "\n"
+                  . "  | o | x" . "\n"
+                  . "--+---+--" . "\n"
+                  . "o |   | x" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testTieGame() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 0, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 0, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 1, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 2, 2));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 2, 0));
+        $expected = "x | o | x" . "\n"
+                  . "--+---+--" . "\n"
+                  . "o | o | x" . "\n"
+                  . "--+---+--" . "\n"
+                  . "x | x | o" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function test5x5Game() {
+        $ticTacToe = new TicTacToe(5);
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 3, 3));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 4, 4));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 3, 4));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 4, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 3, 0));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 3, 1));
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('o', 1, 4));
+        $this->assertEquals('x', $ticTacToe->makeMoveAndGetWinner('x', 3, 2));
+        $expected = "  |   |   |   |  " . "\n"
+                  . "--+---+---+---+--" . "\n"
+                  . "  | o |   |   | o" . "\n"
+                  . "--+---+---+---+--" . "\n"
+                  . "  |   |   |   |  " . "\n"
+                  . "--+---+---+---+--" . "\n"
+                  . "x | x | x | x | x" . "\n"
+                  . "--+---+---+---+--" . "\n"
+                  . "o |   |   |   | o" . "\n";
+        $this->assertEquals($expected, (string) $ticTacToe);
+    }
+
+    public function testIllegalMove() {
+        $ticTacToe = new TicTacToe();
+        $this->assertNull($ticTacToe->makeMoveAndGetWinner('x', 1, 1));
+        $this->setExpectedException('InvalidArgumentException');
+        $ticTacToe->makeMoveAndGetWinner('o', 1, 1);
+    }
+}
