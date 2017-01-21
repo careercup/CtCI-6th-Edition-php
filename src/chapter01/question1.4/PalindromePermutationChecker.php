5b2368546c07eb92f1a159fe0a6f70bce277bbf1
Brad Bernard <bradbernard@me.com>
2017-01-21 11:00:41 -0800

    Removed char counting, space checking, and lowercase casting of chars

--- src/chapter01/question1.4/PalindromePermutationChecker.php
+++ src/chapter01/question1.4/PalindromePermutationChecker.php
@@ -2,23 +2,11 @@
 
 class PalindromePermutationChecker {
     public static function isPalindromePermutation($str) {
-        $charCounts = [];
-        for ($i=0, $length=strlen($str); $i<$length; $i++) {
-            $char = $str[$i];
-            // ignore spaces
-            if ($char === ' ') {
-                continue;
-            }
-            $char = strtolower($char);
-            if (array_key_exists($char, $charCounts)) {
-                $charCounts[$char]++;
-            } else {
-                $charCounts[$char] = 1;
-            }
-        }
+        $str = strtolower(trim($str));
+        $charCounts = array_count_values(str_split($str));
         $oddCount = 0;
         foreach ($charCounts as $char => $count) {
-            if ($count % 2 != 0 && ++$oddCount > 1) {
+            if ($count % 2 !== 0 && ++$oddCount > 1) {
                 return false;
             }
         }
