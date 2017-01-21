01481e13f9e01fde7ceae73070fcc99de46d7674
Brad Bernard <bradbernard@me.com>
2017-01-21 11:03:16 -0800

    Not trim, but str_replace

--- src/chapter01/question1.4/PalindromePermutationChecker.php
+++ src/chapter01/question1.4/PalindromePermutationChecker.php
@@ -2,11 +2,11 @@
 
 class PalindromePermutationChecker {
     public static function isPalindromePermutation($str) {
-        $str = strtolower(trim($str));
+        $str = strtolower(str_replace(' ', '', $str));
         $charCounts = array_count_values(str_split($str));
         $oddCount = 0;
         foreach ($charCounts as $char => $count) {
-            if ($count % 2 !== 0 && ++$oddCount > 1) {
+            if ($count % 2 != 0 && ++$oddCount > 1) {
                 return false;
             }
         }
