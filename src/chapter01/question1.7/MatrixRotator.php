9d0d9855efc97d5c923060dfc0030c7de6a51bfa
asaph <ace@dontletgo.com>
2017-01-23 08:46:21 -0800

    Refactor: better variable names in swapping operation make the code easier to understand. Also, be mindful of integer division in php.

--- src/chapter01/question1.7/MatrixRotator.php
+++ src/chapter01/question1.7/MatrixRotator.php
@@ -2,23 +2,16 @@
 
 class MatrixRotator {
     public static function rotate(array &$matrix) {
-        $length = count($matrix);
         // process 1 layer of the matrix at a time
         // starting from the outside and moving inwards
-        for ($layer=0, $layerCount=$length/2; $layer<$layerCount; $layer++) {
-            $start = $layer;
-            $end = $length - $start - 1;
-            for ($j=$start; $j<$end; $j++) {
+        for ($layer=0, $layers=floor(count($matrix)/2), $begin=0, $end=count($matrix)-1; $layer<$layers; $layer++, $begin++, $end--) {
+            for ($i=$begin, $j=$end; $i<$end; $i++, $j--) {
                 // perform a 4-way swap of values in the current layer
-                $temp = $matrix[$j][$layer];
-
-                $matrix[$j][$layer] = $matrix[$length - $layer - 1][$j];
-
-                $matrix[$length - $layer - 1][$j] = $matrix[$length - $j - 1][$length - $layer - 1];
-
-                $matrix[$length - $j - 1][$length - $layer - 1] = $matrix[$layer][$length - $j - 1];
-
-                $matrix[$layer][$length - $j - 1] = $temp;
+                $tmp = $matrix[$begin][$i];
+                $matrix[$begin][$i] = $matrix[$j][$begin];
+                $matrix[$j][$begin] = $matrix[$end][$j];
+                $matrix[$end][$j] = $matrix[$i][$end];
+                $matrix[$i][$end] = $tmp;
             }
         }
     }
