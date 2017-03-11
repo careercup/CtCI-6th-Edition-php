e1fa34f322245b3201e8549f8653f94068f8c139
asaph <ace@dontletgo.com>
2017-03-10 23:23:41 -0800

    Replace solution to question 2.4 with a more elegant one. The previous solution had a bug which was pointed out in https://github.com/careercup/CtCI-6th-Edition-php/issues/3

--- src/chapter02/question2.4/LinkedListPartitioner.php
+++ src/chapter02/question2.4/LinkedListPartitioner.php
@@ -3,23 +3,26 @@ require_once __DIR__ . '/../../lib/Node.php';
 
 class LinkedListPartitioner {
     public static function partition(Node $node, $x) {
-        $head = $node;
-        $previousNode = null;
+        // build 2 linked lists: one for the values less than $x
+        // and one for the values greater than or equal to $x
+        $lowerHead = new Node(0); // placeholder node at the head
+        $lowerTail = $lowerHead;
+        $upperHead = new Node(0); // placeholder node at the head
+        $upperTail = $upperHead;
         while ($node !== null) {
-            if ($node->getData() < $x && $previousNode !== null) {
-                // remove the node from this part of the list
-                $previousNode->setNext($node->getNext());
-                // put this node at the begining of the list
-                $node->setNext($head);
-                // reset head
-                $head = $node;
-                // reset node for the next iteration
-                $node = $previousNode;
+            if ($node->getData() < $x) {
+                $lowerTail->setNext($node);
+                $lowerTail = $node;
             } else {
-                $previousNode = $node;
-                $node = $node->getNext();
+                $upperTail->setNext($node);
+                $upperTail = $node;
             }
+            $node = $node->getNext();
         }
-        return $head;
+        // attach the upper list to the lower list
+        // and discard the placeholder nodes
+        $lowerTail->setNext($upperHead->getNext());
+        $upperTail->setNext(null);
+        return $lowerHead->getNext();
     }
 }
